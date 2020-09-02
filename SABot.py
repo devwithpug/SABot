9616563806239783eeee52029941fb422ceaba3c
Jungyu Choi <zmfjscl789@gmail.com>
2020-09-03 03:00:42 +0900

    0.1

--- SABot.py
+++ SABot.py
@@ -13,10 +13,20 @@ print("Token_Key : ", token)
 game = discord.Game("상태창 메세지")
 bot = commands.Bot(command_prefix = "!", status = discord.Status.online, activity = game)
 
-#Bot initialization
+#Bot events
 @bot.event
 async def on_ready():
     print("We have logged in as {0.user}".format(bot))
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None and after.channel is not None and after.channel.name != "딸방":
+        embed = discord.Embed(title = member.name + " 님이 " + after.channel.name + " 보이스 채널에 접속했습니다.")
+        await member.guild.system_channel.send(embed = embed)
+    if after.channel is not None and after.channel.name == "딸방":
+        embed = discord.Embed(title = member.name + " 님이 개인적인 시간을 보내러 갔어요. ㅎㅎ;")
+        await member.guild.system_channel.send(embed = embed)
+
+#Bot commands
 @bot.command()
 async def joined(ctx, *, member: discord.VoiceChannel):
     await ctx.send("{0} joined on {0.joined_at}".format(member))
