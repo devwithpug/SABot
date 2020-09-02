60f55fe324512ec1f2b6d98522bad33c988d5c64
Jungyu Choi <zmfjscl789@gmail.com>
2020-09-02 23:19:53 +0900

    0.1

--- SAbot.py
+++ SAbot.py
@@ -3,14 +3,13 @@ import asyncio
 import os
 from discord.ext import commands
 
-#Get token
+#Get Token
 token_path = os.path.dirname(os.path.abspath(__file__)) + "/.token"
 t = open(token_path, "r", encoding="utf-8")
 token = t.read().split()[0]
 print("Token_Key : ", token)
 
 #Bot Settings
-token = "NzQ5NjMyMzExMjA4NzA2MDcz.X0uzfg.5g976c8ed9rTxV4YTUFM0CWTXLs"
 game = discord.Game("상태창 메세지")
 bot = commands.Bot(command_prefix = "!", status = discord.Status.online, activity = game)
 
