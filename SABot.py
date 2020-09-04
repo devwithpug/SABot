0859a13a3b60290f96c4cefaa4ca9951f39c6fe8
Jungyu Choi <zmfjscl789@gmail.com>
2020-09-04 13:25:57 +0900

    0.1

--- SABot.py
+++ SABot.py
@@ -19,10 +19,10 @@ async def on_ready():
     print("We have logged in as {0.user}".format(bot))
 @bot.event
 async def on_voice_state_update(member, before, after):
-    if before.channel is None and after.channel is not None and after.channel.name != "딸방":
+    if before.channel is None and after.channel is not None and after.channel is member.guild.afk_channel:
         embed = discord.Embed(title = member.name + " 님이 " + after.channel.name + " 보이스 채널에 접속했습니다.")
         await member.guild.system_channel.send(embed = embed)
-    if after.channel is not None and after.channel.name == "딸방":
+    if after.channel is not None and after.channel is member.guild.afk_channel:
         embed = discord.Embed(title = member.name + " 님이 개인적인 시간을 보내러 갔어요. ㅎㅎ;")
         await member.guild.system_channel.send(embed = embed)
 
@@ -31,10 +31,26 @@ async def on_voice_state_update(member, before, after):
 async def joined(ctx, *, member: discord.VoiceChannel):
     await ctx.send("{0} joined on {0.joined_at}".format(member))
 @bot.command()
-async def test(ctx):
-    await ctx.send("hello")
+async def debug(ctx):
+    embed = discord.Embed()
+    embed.set_author(name = ctx.guild.name+" Member list")
+    embed.set_thumbnail(url=ctx.guild.icon_url)
+    value = str()
+    for member in ctx.guild.members:
+        if member.nick:
+            value += member.name + " / " + member.nick + "\n"
+        else:
+            value += member.name + " / " + "None" + "\n"
+    embed.add_field(name = "Name / Nickname", value = value, inline = False)
+    await ctx.send(embed = embed)
 @bot.command()
 async def hello(ctx):
     await ctx.send("hiiiiii")
-
+@bot.command()
+async def 엔딩곡(ctx):
+    for channel in ctx.guild.text_channels:
+        if channel.name == "노래":
+            await channel.send(content = '_play 용과같이 바보같이')
+            await ctx.send('_play 용과같이 바보같이')
+    
 bot.run(token)
